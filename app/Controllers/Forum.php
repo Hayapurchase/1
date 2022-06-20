4b7746346394f284a972bc73ceefaf8f0e39be28
Hayapurchase <Eric.Lesilau@strathmore.edu>
2022-06-20 23:14:15 +0300

    forum complete

---
+++ app/Controllers/Forum.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ForumModel;
+use App\Models\CommentModel;
+
+class Forum extends BaseController
+{
+    public function index()
+    {
+      
+        $forums=new ForumModel();
+
+        $data['forum']=json_decode(json_encode($forums
+            ->whereIn('forum.is_deleted',[0])
+            ->join('kids','kids.kid_id=forum.initiator')->paginate()),true);
+
+        // echo "<pre>";
+        // print_r($data);
+
+        // echo "</pre>";
+        //echo view('forum.php',$data);
+        return view('forum',$data);
+    }
+    public function addforum()
+    {
+        $forums=new ForumModel();
+
+        $data=[
+            'initiator'=>'4',
+            'title'=>$this->request->getPost('title'),
+            'genre'=>$this->request->getPost('genre'),
+            'main_comment'=>$this->request->getPost('main_comment')
+        ];
+
+        $forums->save($data);
+
+        return redirect()->to('forumhome');
+
+    }
+    public function forumdetails($id)
+    {
+        $forums=new ForumModel();
+        $comments=new CommentModel();
+
+        $data['forum']=json_decode(json_encode($forums->whereIn('forum.forum_id',[$id])->whereIn('forum.is_deleted',[0])
+            ->join('kids','kids.kid_id=forum.initiator')->paginate()),true);
+        $data['comments']=json_decode(json_encode($comments->whereIn('comments.forum',[$id])->whereIn('comments.is_deleted',[0])->join('kids','kids.kid_id=comments.commentor')->join('forum','comments.forum=forum.forum_id')->paginate()),true);
+
+        return view('comments',$data);
+    }
+     public function addComment($id)
+    {
+        $comment=new CommentModel();
+
+        $data=[
+            'commentor'=>'3',
+            'forum'=>$id,
+            'comment_post'=>$this->request->getPost('comment_post')
+        ];
+
+        $comment->save($data);
+
+        
+
+    }
+
+        public function deleteforum($id)
+    {
+        $forums=new ForumModel();
+
+        $comments=new CommentModel();
+        $commnet_arr=json_decode(json_encode($comments->whereIn('forum', [$id])->whereIn('is_deleted',[0])->paginate()),true);
+        $newData = [
+            'is_deleted'    => 1 ,
+        ];
+        if($commnet_arr!=null){
+            foreach ($commnet_arr as $item){ 
+                $comments->update($item['comment_id'],$newData);
+            }
+            $forums->update($id,$newData);
+            return redirect()->to('forumhome');
+        }else{
+            $forums->update($id,$newData);
+            
+            return redirect()->to('forumhome');
+        }
+       
+        
+    }
+    public function deletecomment($id)
+    {
+        $comments=new CommentModel();
+        $newData = [
+            'is_deleted'    => 1 ,
+        ];
+        $comments->update($id,$newData);
+        return redirect()->to('forumhome');
+    }
+    public function flagcomment($cid,$id)
+    {
+        $comments=new CommentModel();
+        $forums=new ForumModel();
+
+        $newData = [
+            'comments_flagged'    => 1 ,
+        ];
+        $otherdat=[
+            'flagged'    => 1 ,
+        ];
+
+        $forums->update($id,$otherdat);
+        $comments->update($cid,$newData);
+        return redirect()->to('forumhome');
+    }
+    public function unflagcomment($cid,$id)
+    {
+        $comments=new CommentModel();
+        $forums=new ForumModel();
+
+        $newData = [
+            'comments_flagged'    => 0 ,
+        ];
+        $otherdat=[
+            'flagged'    => 0 ,
+        ];
+
+        $forums->update($id,$otherdat);
+        $comments->update($cid,$newData);
+        return redirect()->to('forumhome');
+    }
+    public function flagforum($id)
+    {
+        $forums=new ForumModel();
+
+        $newData = [
+            'flagged'    => 1 ,
+        ];
+
+        $forums->update($id,$newData);
+        return redirect()->to('forumhome');
+    }
+        public function unflagforum($id)
+    {
+        $forums=new ForumModel();
+
+        $newData = [
+            'flagged'    => 0 ,
+        ];
+
+        $forums->update($id,$newData);
+        return redirect()->to('forumhome');
+    }
+   
+}
---
+++ app/Views/Admin/Frontend/Home/adminhome.php
@@ -0,0 +1,8 @@
+<?= $this->extend('Admin/layouts/admin')?>
+<?=$this->section('content')?>
+    <div class="card">
+            <div class="card-body">
+                <h1>Content</h1>
+            </div>
+    </div>
+<?=$this->endSection()?>
